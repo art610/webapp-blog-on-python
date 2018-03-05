8c60df5596d23985885aa3f88895988afbb7f791
Artem Zhelonkin <art610@outlook.com>
2018-03-05 13:37:29 +0300

    transfer from BitBucket

---
+++ blog/blocks.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018
+#
+
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailcore import blocks
+from wagtail.wagtailimages.blocks import ImageChooserBlock
+from wagtail.wagtailembeds.blocks import EmbedBlock
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, FieldRowPanel,MultiFieldPanel, \
+    InlinePanel, PageChooserPanel, StreamFieldPanel
+
+class ColumnBlock(blocks.StreamBlock):
+    heading = blocks.CharBlock(classname="full title")
+    paragraph = blocks.RichTextBlock()
+    image = ImageChooserBlock()
+
+    class Meta:
+        template = 'blog/blocks/column.html'
+
+class TwoColumnBlock(blocks.StructBlock):
+
+    left_column = ColumnBlock(icon='arrow-right', label='Left column content')
+    right_column = ColumnBlock(icon='arrow-right', label='Right column content')
+
+    class Meta:
+        template = 'blog/blocks/two_column_block.html'
+        icon = 'placeholder'
+        label = 'Two Columns'
+
